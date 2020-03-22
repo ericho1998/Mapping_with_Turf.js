8b3cc70c6b3c37851433223a1b990080a413d2b3
Eric Ho <eric.ho@ucalgary.ca>
2020-03-22 17:01:05 -0600

    Completed, but might change background/base map later

---
+++ js/vars.js
@@ -0,0 +1,341 @@
+var clinics = {
+  type: 'FeatureCollection',
+  features: [
+    {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.06391459432675,
+            51.11218619333126
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Thornhill Community Health Centre",
+        "latitude": "51.11218619333126",
+        ":@computed_region_kxmf_bzkv": "261",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.06391459432675",
+        ":@computed_region_hq2j_w7j9": "158",
+        "address": "6617 Centre ST NW",
+        "type": "PHS Clinic",
+        "comm_code": "HUN"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.10650709115143,
+            51.06532159267874
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "North Hill Community Health Centre",
+        "latitude": "51.06532159267874",
+        ":@computed_region_kxmf_bzkv": "152",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.10650709115143",
+        ":@computed_region_hq2j_w7j9": "182",
+        "address": "1527 19 ST NW",
+        "type": "PHS Clinic",
+        "comm_code": "HOU"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.07212956123551,
+            51.04116344159869
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Sheldon M. Chumir Health Centre",
+        "latitude": "51.04116344159869",
+        ":@computed_region_kxmf_bzkv": "260",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.07212956123551",
+        ":@computed_region_hq2j_w7j9": "130",
+        "address": "1213 - 4 ST SW",
+        "type": "PHS Clinic",
+        "comm_code": "BLN"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -113.95355569514419,
+            51.07553892796213
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Village Square Community Health Centre",
+        "latitude": "51.07553892796213",
+        ":@computed_region_kxmf_bzkv": "36",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-113.95355569514419",
+        ":@computed_region_hq2j_w7j9": "294",
+        "address": "2623 - 56 ST NE",
+        "type": "PHS Clinic",
+        "comm_code": "PIN"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -113.9672200855659,
+            51.04504854027524
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "East Calgary Health Centre",
+        "latitude": "51.04504854027524",
+        ":@computed_region_kxmf_bzkv": "85",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-113.9672200855659",
+        ":@computed_region_hq2j_w7j9": "211",
+        "address": "4715 8 AV SE",
+        "type": "PHS Clinic",
+        "comm_code": "FLN"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.0709401043907,
+            50.97507203441003
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Acadia Community Health Centre",
+        "latitude": "50.97507203441003",
+        ":@computed_region_kxmf_bzkv": "174",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.0709401043907",
+        ":@computed_region_hq2j_w7j9": "81",
+        "address": "151 - 86 AV SE",
+        "type": "PHS Clinic",
+        "comm_code": "ACA"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.05857497443219,
+            50.90257527258776
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "South Calgary Health Centre",
+        "latitude": "50.90257527258776",
+        ":@computed_region_kxmf_bzkv": "52",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.05857497443219",
+        ":@computed_region_hq2j_w7j9": "128",
+        "address": "31 Sunpark Plaza SE",
+        "type": "PHS Clinic",
+        "comm_code": "SDC"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.13684189371192,
+            51.04488712410958
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Shaganappi Complex Health Centre",
+        "latitude": "51.04488712410958",
+        ":@computed_region_kxmf_bzkv": "118",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.13684189371192",
+        ":@computed_region_hq2j_w7j9": "259",
+        "address": "3415 - 8 AV SW",
+        "type": "PHS Clinic",
+        "comm_code": "SPR"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.1971615208791,
+            51.123333552258295
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Northwest Community Health Centre",
+        "latitude": "51.123333552258295",
+        ":@computed_region_kxmf_bzkv": "245",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.1971615208791",
+        ":@computed_region_hq2j_w7j9": "174",
+        "address": "109 1829 Ranchlands BV NW",
+        "type": "PHS Clinic",
+        "comm_code": "RAN"
+    }
+},
+{
+ "type": "Feature",
+ "geometry": {
+     "type": "Point",
+     "coordinates": [
+         -114.13217660430655,
+         51.064182937103176
+     ]
+ },
+ "properties": {
+     "location_state": "",
+     "location_zip": "",
+     "name": "Foothills Hospital",
+     "latitude": "51.064182937103176",
+     ":@computed_region_kxmf_bzkv": "154",
+     "location_address": "",
+     "location_city": "",
+     "longitude": "-114.13217660430655",
+     ":@computed_region_hq2j_w7j9": "103",
+     "address": "1403 - 29 ST NW",
+     "type": "Hospital",
+     "comm_code": "STA"
+ }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -113.98394351382268,
+            51.07901634932495
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Peter Lougheed Medical Centre",
+        "latitude": "51.07901634932495",
+        ":@computed_region_kxmf_bzkv": "83",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-113.98394351382268",
+        ":@computed_region_hq2j_w7j9": "223",
+        "address": "3500 - 26 AV NE",
+        "type": "Hospital",
+        "comm_code": "SUN"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.14795761511185,
+            51.07455988033879
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Alberta Children's Hospital",
+        "latitude": "51.07455988033879",
+        ":@computed_region_kxmf_bzkv": "155",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.14795761511185",
+        ":@computed_region_hq2j_w7j9": "104",
+        "address": "2888 Shaganappi Trail NW",
+        "type": "Hospital",
+        "comm_code": "UOC"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -113.95170381192895,
+            50.88210185113848
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "South Health Campus",
+        "latitude": "50.88210185113848",
+        ":@computed_region_kxmf_bzkv": "90",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-113.95170381192895",
+        ":@computed_region_hq2j_w7j9": "197",
+        "address": "4448 FRONT ST SE",
+        "type": "Hospital",
+        "comm_code": "SET"
+    }
+},
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -114.09632069904508,
+            50.991828888017004
+        ]
+    },
+    "properties": {
+        "location_state": "",
+        "location_zip": "",
+        "name": "Rockyview General Hospital",
+        "latitude": "50.991828888017004",
+        ":@computed_region_kxmf_bzkv": "58",
+        "location_address": "",
+        "location_city": "",
+        "longitude": "-114.09632069904508",
+        ":@computed_region_hq2j_w7j9": "56",
+        "address": "7007 - 14 ST SW",
+        "type": "Hospital",
+        "comm_code": "EAG"
+    }
+}
+  ]
+};
